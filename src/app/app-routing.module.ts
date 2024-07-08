467326554be5e888d00972cd6b1696534939fb44
Yared Ekubay <jaredequbay16@gmail.com>
2024-07-08 10:59:28 +0300

    about-us final version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import { ContactUsComponent } from './components/contact-us/contact-us.component
 import { LoginComponent } from './components/login/login.component';
 import { CoursesComponent } from './components/courses/courses.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
-
+import { FooterComponent } from './components/footer/footer.component';
+import { SecondHeaderComponent } from './components/second-header/second-header.component';
 import { SpecificCourseComponent } from './components/courses/specific-course/specific-course.component';
 
 
@@ -16,9 +17,8 @@ const routes: Routes = [
   { path: 'contactus', component: ContactUsComponent },
   { path: 'login', component: LoginComponent },
   { path: 'courses', component: CoursesComponent },
-
-  { path: 'aboutus', component: AboutUsComponent }
-  { path: 'specific-course', component: SpecificCourseComponent },
+  { path: 'aboutus', component: AboutUsComponent },
+  { path: 'specificcourse', component: SpecificCourseComponent },
 ];
 
 @NgModule({
